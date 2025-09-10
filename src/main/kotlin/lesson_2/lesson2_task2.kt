2cfa041767fd636c0287e77653121e08ee2245a1
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-10 21:44:03 +0300

    KS-2-1 the number of purchases in the store

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,12 @@
+package org.exampleter.lesson_2
+
+fun main() {
+    val numberOfOrders: Int = 100
+    val thanksForThePurchase: String = "Благодарим за покупку!"
+    // var numberOfEmployees : Int = 2000
+    var numberOfEmployees: Int = 1999
+
+    println(numberOfOrders)
+    println(thanksForThePurchase)
+    println(numberOfEmployees)
+}
