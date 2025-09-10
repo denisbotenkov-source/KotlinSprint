3a831dbac1c0b1c0a068fc0b5eece08b3b552dc3
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-10 21:57:32 +0300

    KS-2-3 Гагарин

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,15 @@
+package org.exampleter.lesson_2
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println(year)
+    println(hour)
+    println(minute)
+    hour = 10
+    minute = 55
+    print(hour)
+    print(":")
+    print(minute)
+}
