87497666c211ff6ab574b292cd901b6978d19b17
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-11 21:30:26 +0300

    KS-1-2 Доработал и переименовал

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -3,9 +3,9 @@ package org.exampleter.lesson_2
 fun main() {
     val numberOfOrders: Int = 100
     val thanksForThePurchase: String = "Благодарим за покупку!"
-    var numberOfEmployees = 2000
-    println(numberOfEmployees)
-    numberOfEmployees = 1999
+   // var numberOfEmployees = 2000
+   // println(numberOfEmployees)
+    val numberOfEmployees = 1999
 
     println(numberOfOrders)
     println(thanksForThePurchase)
