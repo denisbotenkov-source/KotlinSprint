c5a6ae099f083ffe0d96086f0f8bbd7708d3cfbb
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-11 22:21:28 +0300

    KS-1-5 Секунды в космосе.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
 package org.exampleter
-fun main() {
 
-    }
+fun main() {
+    val secondsInSpace = 6480
+    val hours = secondsInSpace / 3600                  // 1 час = 3600 секунд
+    val minutes = (secondsInSpace % 3600) / 60
+
+    println("Часы: %02d".format(hours))
+    println("Минуты: $minutes")
+
+}
 
 
