bf7485572112f9b06d7be0fce829ca3cd650d190
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-12 20:45:21 +0300

    KS-1-2 Доработал,исправил количество заказов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package org.exampleter
 
 fun main() {
-    val numberOfOrders: Int = 100
+    val numberOfOrders: Int = 75
     val thanksForThePurchase: String = "Благодарим за покупку!"
     var numberOfEmployees = 2000
     // println(numberOfEmployees)
@@ -10,6 +10,6 @@ fun main() {
 
     println(numberOfOrders)
     println(thanksForThePurchase)
-    println(numberOfEmployees)
+
 }
 
