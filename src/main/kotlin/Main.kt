8262e5a98b3666378e2385c60f6b2594fa0d2183
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-12 20:14:19 +0300

    KS-1-4 (Доработал(Вывел переменные в консоль)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,13 @@ package org.exampleter
 fun main() {
     val length: Long = 40_868_600_000
     val age: Byte = 27
-    val partOfDay: Float= 0.075f
-    val seconds: Short= 6480
-    val partOfYear: Double= 2.0547945205479453E-4
-    val apogee: Int= 327000
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+    println(
+        "length = $length," + "age = $age," + "partOfDay = $partOfDay," + "seconds = $seconds," + "partOfYear = $partOfYear," + "apogee = $apogee"
+    )
 }
 
 
