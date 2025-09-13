f49cab4340362c42af0e98c7c218504525a2a0c7
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-13 22:06:06 +0300

    KS-2-3 Время прибытия поезда

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,13 @@
+package org.exampleter.lesson_2
+
+
+fun main() {
+    val departureHour = 9
+    val departureMinute = 39
+    val travelTimeMinutes = 457
+    val departureTotalMinutes = departureHour * 60 + departureMinute
+    val arrivalTotalMinutes = departureTotalMinutes + travelTimeMinutes
+    val arrivalHour = (arrivalTotalMinutes / 60) % 24
+    val arrivalMinute = arrivalTotalMinutes % 60
+    println("Поезд прибывает в: ${"%02d".format(arrivalHour)}:${"%02d".format(arrivalMinute)}")
+}
