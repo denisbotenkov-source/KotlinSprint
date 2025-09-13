0ff5629e931b588b7fd41b5c17569bbb08128933
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-13 13:09:57 +0300

    KS-1-5 Доработал(Вынес константу за меин)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,18 @@
 package org.exampleter
 
 fun main() {
-    val SECONDS_IN_HOUR = 3600
-    val SECONDS_IN_MINUTE = 60
+
     val secondsInSpace = 6480
     val hours = secondsInSpace / SECONDS_IN_HOUR
     val remainingSecondsAfterHours = secondsInSpace % SECONDS_IN_HOUR
     val minutes = remainingSecondsAfterHours / SECONDS_IN_MINUTE
     val seconds = remainingSecondsAfterHours % SECONDS_IN_MINUTE
 
-    println("[${String.format("%02d:%02d:%02d", hours, minutes, seconds)}]")
+    println("[${String.format("%02d:%02d:%02d".format(hours, minutes, seconds))}]")
 }
+
+val SECONDS_IN_HOUR = 3600
+val SECONDS_IN_MINUTE = 60
 
 
 
