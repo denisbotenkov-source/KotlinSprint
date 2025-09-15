c1b6a54a8c7f2875c06481a8c304bfd48680b039
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-15 18:00:19 +0300

    KS-3-3 Доработал

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -1,16 +1,12 @@
 package org.exampleter.lesson_3
 
-import java.util.Scanner
 
 fun main() {
-    val reader = Scanner(System.`in`)
-    println("Enter a number for the multiplication table:")
-    val num = reader.nextInt()
-    println("Enter the maximum number to multiply by:")
-    val res = reader.nextInt()
-    for (i in 1..res) {
-        println(i * num)
+    val firstNumber = 5
+    val maxNumber = 10
+    val result = mutableListOf<Int>()
+    for (i in 1..maxNumber) {
+        result.add ( i * firstNumber)
     }
-
-
+    println(result)
 }
