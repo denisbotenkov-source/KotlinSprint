a46f84c94d33412bafe7dc87340b54c2cc2c5f50
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-15 20:00:54 +0300

    KS-3-3 Исправил отступы

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -1,12 +1,11 @@
 package org.exampleter.lesson_3
 
-
 fun main() {
     val firstNumber = 5
     val maxNumber = 10
     val result = mutableListOf<Int>()
     for (i in 1..maxNumber) {
-        result.add ( i * firstNumber)
+        result.add(i * firstNumber)
     }
     println(result)
 }
