207ae204b922b030be80afae421497dd107b336b
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-15 11:18:58 +0300

    KS-3-4 Шахматы

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,12 @@
+package org.exampleter.lesson_3
+
+fun main(){
+    var whereFrom = "E2"
+    var where = "E4"
+    var number = 1
+    println("${whereFrom}:${where}:${number}")
+    whereFrom = "D2"
+    where = "D3"
+    number = 1
+    println("${whereFrom}:${where}:${number}")
+}
