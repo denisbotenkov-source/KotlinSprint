ab476640f60adcabfa717dade7e919acc77fd1e2
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-16 14:00:22 +0300

    KS-2-4 Доработал

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,9 +3,9 @@ package org.exampleter.lesson_2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val buff = 20
+    val buff = 0.20
     val baffCrystalOre = crystalOre * buff
     val baffIronOre = ironOre * buff
-    println(" + ${baffCrystalOre} к кристаллической руде.")
-    println(" + ${baffIronOre} к железной руде.")
+    println(" + ${baffCrystalOre.toInt()} к кристаллической руде.")
+    println(" + ${baffIronOre.toInt()} к железной руде.")
 }
