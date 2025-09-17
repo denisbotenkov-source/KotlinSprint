3892fc7dc27a6a098c081eba1ccc47648bc426b7
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-17 20:26:10 +0300

    KS-2-4 Доработал(Добавил константу)

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,9 +3,11 @@ package org.exampleter.lesson_2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val buff = 0.20
-    val baffCrystalOre = crystalOre * buff
-    val baffIronOre = ironOre * buff
-    println(" + ${baffCrystalOre.toInt()} к кристаллической руде.")
-    println(" + ${baffIronOre.toInt()} к железной руде.")
+    val buff = 20
+    val transformation = buff / PERCENTAQES
+    val baffCrystalOre = crystalOre * transformation
+    val baffIronOre = ironOre * transformation
+    println(" + ${baffCrystalOre.toInt()} to the crystalline ore.")
+    println(" + ${baffIronOre.toInt()} to iron ore.")
 }
+const val PERCENTAQES = 100.0
