b87346212bf0a52a344ad951103ea9ab7362b2e4
YouDenisÑ <denis.botenkov@mail.ru>
2025-09-18 20:35:07 +0300

    KS-4-5 (Доработал)

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -7,7 +7,16 @@ fun main() {
     val favorableWeatherConditions = readln()!!.toBoolean()
     val minorDamage = readLine()!!.toBoolean()
     val checkingTheShip =
-        (!damageToTheHousing && currentCrewComposition in 55..70 && cratesOfProvisions > 50) ||
-                (damageToTheHousing && minorDamage && currentCrewComposition == 70 && favorableWeatherConditions)
+        (damageToTheHousing != SHIP_DAMAGED && currentCrewComposition in MIN_CREW_SIZE..MAX_CREW_SIZE && cratesOfProvisions > MIN_PROVISIONS_CRATES) ||
+                (damageToTheHousing == SHIP_DAMAGED && minorDamage == MINOR_DAMAGE_PRESENT && currentCrewComposition == MAX_CREW_SIZE && favorableWeatherConditions == WEATHER_FAVORABLE)
     println("Научно-исследовательский корабль может приступить к долгосрочному плаванию: $checkingTheShip")
 }
+
+const val MIN_CREW_SIZE = 55
+const val MAX_CREW_SIZE = 70
+const val MIN_PROVISIONS_CRATES = 50
+const val SHIP_DAMAGED = true
+const val WEATHER_FAVORABLE = true
+const val MINOR_DAMAGE_PRESENT = true
+
+
