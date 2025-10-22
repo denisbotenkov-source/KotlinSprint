2cd46ab30e3d60efd2e48168b304433f72aeaf3c
YouDenisÑ <denis.botenkov@mail.ru>
2025-10-22 17:22:59 +0300

    KS-5-1 Проверка на возраст

---
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -0,0 +1,14 @@
+package org.exampleter.lesson_5
+
+fun main() {
+    val a = 4
+    val b = 7
+    println("Solve the example:$a+$b")
+    val user = readln().toInt()
+    if (user == 11) {
+        println("Verification completed.")
+    } else {
+        println("Verification failed.")
+    }
+
+}
