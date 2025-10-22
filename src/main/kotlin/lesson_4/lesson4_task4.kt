d39caeba37a5247509b185d86d393b82ce71bdd4
YouDenisÑ <denis.botenkov@mail.ru>
2025-10-22 09:37:34 +0300

    KS-4-4 Переработал код

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,24 +1,18 @@
 package org.exampleter.lesson_4
 
+import java.time.LocalDate
+
 fun main() {
-    var hands = true
-    var legs = false
-    var back = false
-    var press = true
-    val correctDay = 2
-   val isdayEven = correctDay % 2 == 0
-   if (isdayEven){
-       println(
-           """
-            Exercises for the hands:$hands" 
-            Leg exercises:$legs 
-            Exercise for the back:$back 
-            Exercises for the press:$press
-             """.trimMargin())
-   }else{
-       hands = !hands
-       legs = !legs
-       back = !back
-       press = !press
-   }
-       }
+    val day = LocalDate.now().dayOfMonth
+    val handsAndPress = day % 2 != 0
+    val legsAndBack = !handsAndPress
+
+    println(
+        """
+        |”пражнени€ дл€ рук:    $handsAndPress
+        |”пражнени€ дл€ ног:    $legsAndBack
+        |”пражнени€ дл€ спины:  $legsAndBack
+        |”пражнени€ дл€ пресса: $handsAndPress
+        """.trimMargin()
+    )
+}
