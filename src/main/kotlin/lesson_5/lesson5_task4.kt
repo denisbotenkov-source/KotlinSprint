6de245e65c33e86f5d68a3b21dcac16247b7b21c
YouDenisÑ <denis.botenkov@mail.ru>
2025-10-24 10:46:59 +0300

    KS-5-4 Верификация

---
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -0,0 +1,16 @@
+package org.exampleter.lesson_5
+
+fun main() {
+    val userZaphod = "Zaphod"
+    val passvord = "PanGalactic"
+    if (userZaphod == REGISTERED_USER && passvord == REGISTERED_PASSWORD) {
+        println("Hello,Zaphod!")
+    } else if (userZaphod == REGISTERED_USER && passvord != REGISTERED_PASSWORD) {
+        println("Complete the registration process.")
+    } else if (userZaphod != REGISTERED_USER) {
+        println("There is no such name")
+    }
+}
+
+const val REGISTERED_USER = "Zaphod"
+const val REGISTERED_PASSWORD = "PanGalactic"
