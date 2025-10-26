c1f13ba75daa08126d349483f7dcd8f8548027d3
YouDenisÑ <denis.botenkov@mail.ru>
2025-10-26 23:47:22 +0300

    KS-5-6(Исправил)

--- src/main/kotlin/lesson_5/lesson5_task6.kt
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -7,8 +7,13 @@ fun main() {
     val calculation = userHeightCm / 100
     val bmi = userWeight / (calculation * calculation)
     println("Your body mass index: %.2f".format(bmi))
-    if (bmi < 18.5) println("Insufficient body weight.")
-    else if (bmi > 18.5 && bmi < 25 ) println("Normal body weight.")
-    else if (bmi > 25 && bmi < 30) println("Excess body weight.")
-    else if (bmi >= 30) println("Fatness.")
+    if (bmi < 18.5) {
+        println("Insufficient body weight.")
+    } else if (bmi < 25.0) {
+        println("Normal body weight.")
+    } else if (bmi < 30.0) {
+        println("Excess body weight.")
+    } else {
+        println("Fatness.")
+    }
 }
