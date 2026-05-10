3d17b03b40af740d093e274db9593c94a9c9bd0a
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-10 15:50:48 +0300

    KS-3-1: Строки — приветствие в приложении

---
+++ src/main/kotlin/kotlinPracticumLesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.exampleter.kotlinPracticumLesson_3
+
+
+fun main() {
+    val userName = "Иван"
+    var greeting = "Добрый день"
+    println("$greeting, $userName.")
+    greeting = "Добрый вечер"
+    println("$greeting, $userName.")
+}
