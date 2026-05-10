0f32fc19e44a41f9e631205ab57527b74f7bc948
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-10 16:08:31 +0300

    KS-3-2: Конкатенация — смена фамилии

---
+++ src/main/kotlin/kotlinPracticumLesson_3/lesson3_task2.kt
@@ -0,0 +1,12 @@
+package org.exampleter.kotlinPracticumLesson_3
+
+fun main() {
+    var userAge = 20
+    val name = "Татьяна"
+    var surname = "Андреева"
+    val middleName = "Сергеевна"
+    println("$surname $name $middleName, возраст:$userAge")
+    userAge = 22
+    surname = "Сидорова"
+    println("$surname $name $middleName, возраст:$userAge")
+}
