14eb295c22725556d7d9bcb439a2bf23309e478b
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-10 21:50:44 +0300

    KS-3-4: Методы строки — ход в шахматах

---
+++ src/main/kotlin/kotlinSprintLesson_3/lesson3_task4.kt
@@ -0,0 +1,12 @@
+package org.exampleter.kotlinSprintLesson_3
+
+fun main() {
+    var from = "E2"
+    var to = "E4"
+    var strokeNumber = 1
+    println("’од из $from в $to, номер хода $strokeNumber.")
+    from = "D2"
+    to = "D3"
+    strokeNumber = 2
+    println("’од из $from в $to, номер хода $strokeNumber.")
+}
