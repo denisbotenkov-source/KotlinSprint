0009ec5d357e09ca91ec4115786d3c347e39bc16
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-10 21:31:17 +0300

    KS-3-3: String templates — таблица умножения

---
+++ src/main/kotlin/kotlinSprintLesson_3/lesson3_task3.kt
@@ -0,0 +1,17 @@
+package org.exampleter.kotlinSprintLesson_3
+
+fun main() {
+    val num = 6
+    println(
+        "$num * 1 = ${num * 1}\n" +
+                "$num * 2 = ${num * 2}\n" +
+                "$num * 3 = ${num * 3}\n" +
+                "$num * 4 = ${num * 4}\n" +
+                "$num * 5 = ${num * 5}\n" +
+                "$num * 6 = ${num * 6}\n" +
+                "$num * 7 = ${num * 7}\n" +
+                "$num * 8 = ${num * 8}\n" +
+                "$num * 9 = ${num * 9}"
+    )
+
+}
