7a83340f8d1752651c2098bb857d118ae5e8a6c9
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-11 21:12:02 +0300

    KS-3-5: split — парсер сервера ИСправил.ПРисвоил значения переменным не нарушая принцип DRY.

--- src/main/kotlin/kotlinSprintLesson_3/lesson3_task5.kt
+++ src/main/kotlin/kotlinSprintLesson_3/lesson3_task5.kt
@@ -3,9 +3,6 @@ package org.exampleter.kotlinSprintLesson_3
 fun main() {
     val string = "D2-D4;0"
     val result = string.split("-", ";")
-    println(result[0])
-    println(result[1])
-    println(result[2])
     val from = result[0]
     val to = result[1]
     val strokeNumber = result[2]
