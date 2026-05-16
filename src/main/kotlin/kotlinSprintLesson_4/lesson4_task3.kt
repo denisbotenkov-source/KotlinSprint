9baed61d4eb6bf6802abd01b5a5249b09ddc5635
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-16 18:53:59 +0300

    KS-4-3: Логика && и || — мониторинг растений(Доработал)

--- src/main/kotlin/kotlinSprintLesson_4/lesson4_task3.kt
+++ src/main/kotlin/kotlinSprintLesson_4/lesson4_task3.kt
@@ -1,13 +1,15 @@
 package org.exampleter.kotlinSprintLesson_4
 
-fun main(){
+const val HUMIDITY = 20
+const val SEASON_NOW = "Зима"
+fun main() {
     val isAvailabilityOfSunnyWeather: Boolean = true
     val isOutdoorAwning: Boolean = true
+    val airHumidity = 20
+    val season = "Не зима"
 
     val checkingConditions: Boolean =
-        isAvailabilityOfSunnyWeather && isOutdoorAwning && AIR_HUMIDITY == 20 && SEASON != "Зима"
+        isAvailabilityOfSunnyWeather && isOutdoorAwning && airHumidity == HUMIDITY && season != SEASON_NOW
     println("Благоприятные ли условия сейчас для роста бобовых?${checkingConditions}")
 }
 
-const val AIR_HUMIDITY = 20
-const val SEASON = "Зима"
