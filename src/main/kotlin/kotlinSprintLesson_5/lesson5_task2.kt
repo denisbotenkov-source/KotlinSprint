a35c9837fc93c0f9831d362eca3dcb0ac0382e9c
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-17 21:46:34 +0300

    КС-5-2: if-else — проверка совершеннолетия(Изменил названия переменных)

--- src/main/kotlin/kotlinSprintLesson_5/lesson5_task2.kt
+++ src/main/kotlin/kotlinSprintLesson_5/lesson5_task2.kt
@@ -6,8 +6,8 @@ const val COMING_OF_AGE = 18
 
 fun main() {
     println("Введите ваш год рождения:")
-    val year = LocalDate.now().year
-    val user: Int = readln().toInt()
-    val calculation = year - user
-    if (calculation >= COMING_OF_AGE) println("Показать экран со скрытым контентом.")
+    val currentYear = LocalDate.now().year
+    val birthYear: Int = readln().toInt()
+    val age = currentYear - birthYear
+    if (age >= COMING_OF_AGE) println("Показать экран со скрытым контентом.")
 }
