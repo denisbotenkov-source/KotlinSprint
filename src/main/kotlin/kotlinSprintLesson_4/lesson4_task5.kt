594317ed71f97bc6f296099f0d639ea5a30b52f1
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-17 14:06:10 +0300

    KS-4-5: Составные условия — отплытие корабля.Изменил названия переменных,изменил readline() на readln().

--- src/main/kotlin/kotlinSprintLesson_4/lesson4_task5.kt
+++ src/main/kotlin/kotlinSprintLesson_4/lesson4_task5.kt
@@ -2,15 +2,15 @@ package org.exampleter.kotlinSprintLesson_4
 
 fun main() {
     println("Имеет ли корабль повреждения?")
-    val isShipDamage: Boolean = readLine()?.toBoolean() ?: false
+    val isShipDamage: Boolean = readln()?.toBoolean() ?: false
     println("Количество людей на борту?")
-    val currentSquad = readLine()?.toInt() ?: 0
+    val currentSquad = readln()?.toInt() ?: 0
     println("Количество ящиков на борту?")
-    val numberOfMailboxes = readLine()?.toInt() ?: 0
+    val numberOfBoxes = readln()?.toInt() ?: 0
     println("Благоприятна ли погода?")
-    val isFavorableWeatherConditions: Boolean = readLine()?.toBoolean() ?: true
-    val message = (!isShipDamage && currentSquad in 55..70 && numberOfMailboxes > 50) ||
-            (isShipDamage && currentSquad == 70 && numberOfMailboxes >= 50 && isFavorableWeatherConditions)
-    println("Корабль готов к отплытию. $message")
+    val isFavorableWeatherConditions: Boolean = readln()?.toBoolean() ?: true
+    val isReadyToSail = (!isShipDamage && currentSquad in 55..70 && numberOfBoxes > 50) ||
+            (isShipDamage && currentSquad == 70 && numberOfBoxes >= 50 && isFavorableWeatherConditions)
+    println("Корабль готов к отплытию. $isReadyToSail")
 
 }
