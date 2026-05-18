e43e0b898ba1ebf5d71e291f07766c2ed839215e
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-18 21:06:35 +0300

    КС-4-5: Составные условия — отплытие корабля(исправил)

--- src/main/kotlin/kotlinSprintLesson_4/lesson4_task5.kt
+++ src/main/kotlin/kotlinSprintLesson_4/lesson4_task5.kt
@@ -1,15 +1,19 @@
 package org.exampleter.kotlinSprintLesson_4
 
+const val CREW_MIN = 55
+const val CREW_MAX = 70
+const val PROVISION_MIN = 50
+
 fun main() {
     println("Имеет ли корабль повреждения?")
-    val isShipDamage: Boolean = readln()?.toBoolean() ?: false
+    val isShipDamage: Boolean = readln().toBoolean()
     println("Количество людей на борту?")
     val currentSquad = readln()?.toInt() ?: 0
     println("Количество ящиков на борту?")
     val numberOfBoxes = readln()?.toInt() ?: 0
     println("Благоприятна ли погода?")
     val isFavorableWeatherConditions: Boolean = readln()?.toBoolean() ?: true
-    val isReadyToSail = (!isShipDamage && currentSquad in 55..70 && numberOfBoxes > 50) ||
+    val isReadyToSail = (!isShipDamage && currentSquad in CREW_MIN..CREW_MAX && numberOfBoxes > PROVISION_MIN) ||
             (isShipDamage && currentSquad == 70 && numberOfBoxes >= 50 && isFavorableWeatherConditions)
     println("Корабль готов к отплытию. $isReadyToSail")
 
