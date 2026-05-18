c846ff24f8b8c58fa36bdfebaa9e5a58705ed406
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-18 21:28:19 +0300

    KS-5-4: when — корабль Heart of Gold(Исправил по замечаниям)

--- src/main/kotlin/kotlinSprintLesson_5/lesson5_task4.kt
+++ src/main/kotlin/kotlinSprintLesson_5/lesson5_task4.kt
@@ -1,20 +1,25 @@
-package org.exampleter.kotlinSprintLesson_5
+package org.example.kotlinSprintLesson_5
+
+const val USER_NAME = "Zaphod"
+const val PASSWORD = "PanGalactic"
 
 fun main() {
-    val userName = "Zaphod"
-    val password = "PanGalactic"
-    println("Введите своё имя и пароль.")
+    println("Введите своё имя .")
     val user = readln().lowercase()
+    if (user == USER_NAME.lowercase()) {
+        println("Введите пароль")
+    } else println("Пользователь не зарегистрирован.")
     val userPassword = readln().lowercase()
-    if (user == userName.lowercase() && userPassword == password.lowercase()) {
+    if (userPassword == PASSWORD.lowercase()) {
         println(
             "Ваши данные проверены, и о, чудо, они верны... " +
-                    "Пользователь \"$user\", вам разрешено входить на борт корабля \"Heart of Gold\". " +
-                    "Хотя мне всё равно... Ну вперед, войдите... Если вам так уж надо, в конце концов... Меланхолический вздох. " +
-                    "Надеюсь, вам понравится пребывание здесь больше, чем мне."
+                    "Пользователь \"Zaphod\", вам разрешено входить на борт корабля \"Heart of Gold\". " +
+                    "Хотя мне всё равно... Ну вперед, войдите... Если вам так уж надо, в конце концов... Меланхолический вздох." +
+                    " Надеюсь, вам понравится пребывание здесь больше, чем мне."
         )
     } else {
-        println("Пройдите регистрацию.")
+        println("Неверный пароль.")
     }
-
 }
+
+
