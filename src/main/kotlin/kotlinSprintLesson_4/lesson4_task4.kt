cb8adf9753edd2df82b80a6ed3ede78190ddf4bf
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-18 21:02:07 +0300

    KS-4-4: Инверсия ! — программа тренировок(исправил)

--- src/main/kotlin/kotlinSprintLesson_4/lesson4_task4.kt
+++ src/main/kotlin/kotlinSprintLesson_4/lesson4_task4.kt
@@ -1,24 +1,14 @@
-package org.exampleter.kotlinSprintLesson_4
+package org.example.kotlinSprintLesson_4
 
 fun main() {
-    var day = 5
-    var isEvenDay: Boolean = day % 2 == 0
+    val day = 5
+    val isEvenDay: Boolean = day % 2 == 0
     println(
         """
          Упражнения для рук:${!isEvenDay}
          Упражнения для ног:$isEvenDay
          Упражнения для спины:$isEvenDay
          Упражнения для пресса:${!isEvenDay}
-        """
-    )
-    day++
-    isEvenDay = day % 2 == 1
-    println(
-        """
-           Упражнения для рук: $isEvenDay
-           Упражнения для ног: ${!isEvenDay}
-           Упражнения для спины: ${!isEvenDay}
-           Упражнения для пресса: $isEvenDay
         """
     )
 }
