65897224671d7e14ec7d6d1222483af3c866e9c6
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-23 21:18:57 +0300

    KS-6-2: Счётчик while — консольный таймер(Доработал)

--- src/main/kotlin/kotlinSprintLesson_6/lesson6_task2.kt
+++ src/main/kotlin/kotlinSprintLesson_6/lesson6_task2.kt
@@ -2,10 +2,7 @@ package org.exampleter.kotlinSprintLesson_6
 
 fun main() {
     println("Введите количество секунд.")
-    var userSec = readln().toIntOrNull() ?: 0
-    var sec = 1
-    while (userSec >= sec) {
-        println("Прошло ${sec++} секунд.")
-        Thread.sleep(1000)
-    }
+    val userSec = readln().toIntOrNull() ?: 0
+    Thread.sleep(userSec * 1000L)
+        println("Прошло $userSec секунд.")
 }
