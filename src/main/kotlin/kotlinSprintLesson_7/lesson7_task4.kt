3f35610a54bf46c6cc277030f8f5eed2c4493dda
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-25 22:31:31 +0300

    KS-7-4: for по коллекции — таймер(Доработал по замечаниям)

--- src/main/kotlin/kotlinSprintLesson_7/lesson7_task4.kt
+++ src/main/kotlin/kotlinSprintLesson_7/lesson7_task4.kt
@@ -2,12 +2,10 @@ package org.exampleter.kotlinSprintLesson_7
 
 fun main() {
     println("Введите количество секунд.")
-    val user = readln().toIntOrNull()
-    if (user != null) {
-        for (i in user downTo 1) {
-            println("Осталось секунд $i")
-            Thread.sleep(1000)
-        }
+    val seconds = readln().toInt()
+    for (i in seconds downTo 1) {
+        println("Осталось секунд $i")
+        Thread.sleep(1000)
     }
     println("Время вышло.")
 }
