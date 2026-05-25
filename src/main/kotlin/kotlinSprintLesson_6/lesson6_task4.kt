f81b797d41387c8fcebd93b457a2c2b237f284d8
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-25 21:16:09 +0300

    KS-6-4: break — угадай число(доработал)

--- src/main/kotlin/kotlinSprintLesson_6/lesson6_task4.kt
+++ src/main/kotlin/kotlinSprintLesson_6/lesson6_task4.kt
@@ -1,22 +1,19 @@
-package org.exampleter.kotlinSprintLesson_6
+package kotlinSprintLesson_6
+
 
 fun main() {
-    val secretNumber = 6
+    val secretNumber = (1..9).random()
     var attempts = 5
     while (attempts != 0) {
-        println("Угадайте число от 1 до 9.Количество попыток $attempts")
+        println("Угадайте число от 1 до 9. Количество попыток $attempts")
         val user = readln().toIntOrNull()
-        if (user == secretNumber) {
+        if (user == secretNumber && user != null) {
             println("Это была великолепная игра!")
             break
-        }
-        if (user != secretNumber) {
+        } else {
             attempts--
-            println("Неверно.Осталось попыток $attempts")
-        }
-        if (attempts == 0) {
-            println("Было загадано чтсало $secretNumber")
-            break
+            println("Неверно. Осталось попыток $attempts")
         }
     }
+    println("Было загадано число $secretNumber")
 }
