5389b27d9f8734c557fb992507324097ed44d2c5
YouDenisÑ <denis.botenkov@mail.ru>
2026-05-28 23:17:30 +0300

    KS-8-4: IntArray — список ингредиентов(Доработал)

--- src/main/kotlin/kotlinSprintLesson_8/lesson8_task4.kt
+++ src/main/kotlin/kotlinSprintLesson_8/lesson8_task4.kt
@@ -1,18 +1,18 @@
 package kotlinSprintLesson_8
 
 fun main() {
-    val ingridients =
+    val ingredients =
         arrayOf("Свёкла", "Капуста", "Морковь", "Лук", "Картофель", "Мясо", "Томат").map { it.lowercase() }
             .toTypedArray()
-    println(ingridients.joinToString())
+    println(ingredients.joinToString())
     println("Какой ингредиент хотите заменить?")
     val user = readln().lowercase()
-    val check = ingridients.indexOf(user)
-    if (check != -1) {
+    val check = ingredients.indexOf(user)
+    if (check >= 0) {
         println("На какой ингредиент хотите заменить?")
-        val newList = readln().lowercase()
-        ingridients[check] = newList
-        println("Готово! Вы сохранили следующий список: ${ingridients.joinToString()}")
+        val newIngredient = readln().lowercase()
+        ingredients[check] = newIngredient
+        println("Готово! Вы сохранили следующий список: ${ingredients.joinToString()}")
     } else {
         println("Такого ингредиента нет в списке.")
     }
