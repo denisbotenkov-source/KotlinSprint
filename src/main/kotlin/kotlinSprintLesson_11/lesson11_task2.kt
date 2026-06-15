019bf6c252ba8b1db9314aca74f9ee2e8806412f
YouDenisÑ <denis.botenkov@mail.ru>
2026-06-15 19:40:12 +0300

    KS-11-2: Свойства — профиль пользователя(Доработал)

--- src/main/kotlin/kotlinSprintLesson_11/lesson11_task2.kt
+++ src/main/kotlin/kotlinSprintLesson_11/lesson11_task2.kt
@@ -2,30 +2,31 @@ package org.exampleter.kotlinSprintLesson_11
 
 fun main() {
     val user = User2(1, "denkov", "admin", "denkov@mail.ru")
-    user.schitvanieIzKonsoli()
-    user.izmenenieParolya()
-    user.infaUser()
+    user.readBio()
+    user.changePassword()
+    user.displayInfo()
 }
 
 class User2(
-    var id: Int,
+    val id: Int,
     val login: String,
     var password: String,
     val email: String,
-    var bio: String = ""
+    var bio: String? = null
 ) {
-    fun infaUser() = println("$id, $login, $password, $email, $bio")
+    fun displayInfo() = println("$id, $login, $password, $email, ${bio ?: ""}")
 
-    fun schitvanieIzKonsoli(): String {
+    fun readBio(): String? {
         println("О себе:")
         bio = readln()
         return bio
+
     }
 
-    fun izmenenieParolya() {
+    fun changePassword() {
         println("Введите текущий пароль")
-        val user = readln()
-        if (user == password) {
+        val currentPassword = readln()
+        if (currentPassword == password) {
             println("Введите новый пароль.")
             password = readln()
             println("Пароль изменён.")
