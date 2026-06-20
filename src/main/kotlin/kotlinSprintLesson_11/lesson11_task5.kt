bfe360ca17d7e66c2622c55941524e02a211d383
YouDenisÑ <denis.botenkov@mail.ru>
2026-06-20 20:33:11 +0300

    KS-11-5: Список объектов — Forum

---
+++ src/main/kotlin/kotlinSprintLesson_11/lesson11_task5.kt
@@ -0,0 +1,53 @@
+package org.example.kotlinSprintLesson_11
+
+fun main() {
+    val forum = Forum(mutableListOf(), mutableListOf())
+    forum.createNewUser("Анна")
+    forum.createNewMessage(1, "Всем привет!")
+    forum.createNewUser("Борис")
+    forum.createNewMessage(2, "Как дела?")
+    forum.createNewMessage(1, "Рад всех видеть!")
+    forum.createNewMessage(2, "Всё отлично, спасибо!")
+    forum.printThread()
+}
+
+class Forum(
+    val members: MutableList<ForumMember>,
+    val messages: MutableList<ForumMessage>,
+) {
+
+    fun createNewUser(userName: String): ForumMember {
+        val newId = members.size + 1
+        val newMember = ForumMember(newId, userName)
+        members.add(newMember)
+        return newMember
+    }
+
+    fun createNewMessage(authorId: Int, message: String): ForumMessage? {
+        val user = members.find { it.userId == authorId }
+        if (user != null) {
+            val newMessage = ForumMessage(authorId, message)
+            messages.add(newMessage)
+            return newMessage
+        }
+        return null
+    }
+
+    fun printThread() {
+        for (msg in messages) {
+            val author = members.find { it.userId == msg.authorId }
+            val userName = author?.userName ?: "Неизвестный"
+            println("$userName: ${msg.message}")
+        }
+    }
+
+    class ForumMember(
+        var userId: Int,
+        var userName: String,
+    )
+
+    class ForumMessage(
+        val authorId: Int,
+        val message: String,
+    )
+}
