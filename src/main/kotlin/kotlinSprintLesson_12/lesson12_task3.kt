775ab554abc3bbc41c435aa93ce91d890f05bd71
YouDenisÑ <denis.botenkov@mail.ru>
2026-06-24 21:23:16 +0300

    KS-12-3: Secondary constructor — погода в Кельвинах(Доработал)

--- src/main/kotlin/kotlinSprintLesson_12/lesson12_task3.kt
+++ src/main/kotlin/kotlinSprintLesson_12/lesson12_task3.kt
@@ -1,14 +1,18 @@
 package org.exampleter.kotlinSprintLesson_12
 
+const val CELSIUM_ED = 273
+
 fun main() {
     val converter = Converter(300)
     converter.print()
 }
 
 class Converter(kelvin: Int) {
-    val celsius = kelvin - 273
+    val celsius = kelvin - CELSIUM_ED
 
     fun print() {
         println("$celsius")
     }
 }
+
+//Я не понимаю для чего тут копировать класс с предыдущего урока, если там был вывод температуры в консоль. а здесь просто перевод из кельвинов в цельсии((
