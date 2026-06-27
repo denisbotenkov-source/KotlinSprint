365b4b41e1e76359ce0a1972297b89e76d3aedee
YouDenisÑ <denis.botenkov@mail.ru>
2026-06-27 18:27:39 +0300

    KS-13-3: Elvis ?: — значение по умолчанию

---
+++ src/main/kotlin/kotlinSprintLesson_13/lesson13_task3.kt
@@ -0,0 +1,18 @@
+package org.exampleter.kotlinSprintLesson_13
+
+fun main() {
+    val contacts = listOf(
+        ContactNumber("Имя1", 89123456789, "Компания"),
+        ContactNumber("Имя2", 89123456780, null),
+        ContactNumber("Имя3", 89123456781, "null"),
+        ContactNumber("Имя4", 89123456782, null),
+        ContactNumber("Имя5", 89123456783, "ДругаяКомпания")
+    )
+    println("${contacts.map { it.companyName ?: "Какая то компания" }}")
+}
+
+class ContactNumber(
+    val name: String,
+    val phoneNumber: Long,
+    val companyName: String? = null
+)
