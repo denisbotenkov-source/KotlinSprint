a6b0bd50a753ed45fb00e0399d40b06a66c3039d
YouDenisÑ <denis.botenkov@mail.ru>
2026-06-27 20:06:46 +0300

    KS-12-4: require() — вывод в init(Доработал)

--- src/main/kotlin/kotlinSprintLesson_12/lesson12_task4.kt
+++ src/main/kotlin/kotlinSprintLesson_12/lesson12_task4.kt
@@ -2,13 +2,15 @@ package org.exampleter.kotlinSprintLesson_12
 
 const val CELSIUM_ED = 273
 fun main() {
-    val converter = Converter(300)
+    val converter = WeatherV3(300, 310, 320, true)
 
 }
 
-class Converter(kelvin: Int) {
+class WeatherV3(kelvin: Int, dayTemperature: Int, nightTemperature: Int, hasPrecipitation: Boolean) {
     init {
         val celsius = kelvin - CELSIUM_ED
-        println(celsius)
+        val dayTemp = dayTemperature - CELSIUM_ED
+        val nightTemp = nightTemperature - CELSIUM_ED
+        println("$celsius, $dayTemp, $nightTemp, $hasPrecipitation")
     }
 }
