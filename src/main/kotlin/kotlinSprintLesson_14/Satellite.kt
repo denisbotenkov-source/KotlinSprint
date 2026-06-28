d01bd93d9d88b4ee514ee6b6bd80c8f399b89232
YouDenisÑ <denis.botenkov@mail.ru>
2026-06-28 20:49:24 +0300

    KS-14-4: Цепочка наследования — планеты

---
+++ src/main/kotlin/kotlinSprintLesson_14/Satellite.kt
@@ -0,0 +1,5 @@
+package org.exampleter.kotlinSprintLesson_14
+
+class Satellite(name: String, suitableForPlanting: Boolean, autmosphere: Boolean) :
+    CelestialBody(name, suitableForPlanting, autmosphere) {
+}
