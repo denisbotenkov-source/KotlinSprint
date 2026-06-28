6c913c8219073072f3315cec83205d17ea863300
YouDenisÑ <denis.botenkov@mail.ru>
2026-06-28 22:35:51 +0300

    KS-14-4: Цепочка наследования — планеты(Убрал квадратные скобки в выводе)

--- src/main/kotlin/kotlinSprintLesson_14/lesson14_task4.kt
+++ src/main/kotlin/kotlinSprintLesson_14/lesson14_task4.kt
@@ -3,5 +3,5 @@ package org.exampleter.kotlinSprintLesson_14
 fun main() {
     val planet =
         Planet("Марс", true, false, listOf(Satellite("Фобос", false, true), Satellite("Деймос", false, true)))
-    println("Планета: ${planet.name},спутники: ${planet.spisokSputnikov.map { it.name }}")
+    println("Планета: ${planet.name},спутники: ${planet.spisokSputnikov.joinToString { it.name }}")
 }
