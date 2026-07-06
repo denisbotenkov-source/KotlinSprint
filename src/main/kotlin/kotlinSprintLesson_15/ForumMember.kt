6f1349b8c9e1bceee148491dd19dedd3a51d61aa
YouDenisÑ <denis.botenkov@mail.ru>
2026-07-06 20:19:35 +0300

    КС-15-3: несколько интерфейсов — пользователи форума и администраторы(Доработал)

--- src/main/kotlin/kotlinSprintLesson_15/ForumMember.kt
+++ src/main/kotlin/kotlinSprintLesson_15/ForumMember.kt
@@ -1,10 +1,10 @@
 package org.exampleter.kotlinSprintLesson_15
 
 abstract class ForumMember(val name: String) {
-    fun read(){
+    fun read() {
         println("$name читает форум.")
     }
-    fun writeMessage(){
+    fun writeMessage() {
         println("$name печатает сообщение.")
     }
 }
--- src/main/kotlin/kotlinSprintLesson_15/User.kt
+++ src/main/kotlin/kotlinSprintLesson_15/User.kt
@@ -1,4 +1,3 @@
 package org.exampleter.kotlinSprintLesson_15
 
-class User( name: String) : ForumMember(name) {
-}
+class User( name: String) : ForumMember(name)
