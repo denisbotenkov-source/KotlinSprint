57106feca6b491b6e828abb7eb61d2cd2965ea4e
YouDenisÑ <denis.botenkov@mail.ru>
2026-07-10 23:28:37 +0300

    KS-16-4: encapsulation logic — заказ в интернет-магазине

---
+++ src/main/kotlin/kotlinSprintLesson_16/Order.kt
@@ -0,0 +1,12 @@
+package org.exampleter.kotlinSprintLesson_16
+
+class Order(private val orderNumber: Int, var status: String) {
+    fun sendRequest(newStatus: String) {
+        updateStatus(newStatus)
+    }
+
+    private fun updateStatus(newStatus: String) {
+        status = newStatus
+        println("Статус $newStatus подтверждён. ")
+    }
+}
---
+++ src/main/kotlin/kotlinSprintLesson_16/lesson16_task4.kt
@@ -0,0 +1,6 @@
+package org.exampleter.kotlinSprintLesson_16
+
+fun main() {
+    val order = Order(2, "В работе")
+    order.sendRequest("Готов")
+}
