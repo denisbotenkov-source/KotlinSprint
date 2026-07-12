8ed1b32b4f5612f6239a3c96bf1afb5aff842c2c
YouDenisÑ <denis.botenkov@mail.ru>
2026-07-12 23:08:57 +0300

    KS-16-5: private constructor — игрок

---
+++ src/main/kotlin/kotlinSprintLesson_16/lesson16_task5.kt
@@ -0,0 +1,14 @@
+package org.exampleter.kotlinSprintLesson_16
+
+fun main() {
+val player = Player("Denis", 100, 30)
+    player.dealingDamage()
+    player.dealingDamage()
+    player.healthePlayer(30)
+    player.healthePlayer(30)
+    player.dealingDamage()
+    player.dealingDamage()
+    player.dealingDamage()
+    player.dealingDamage()
+
+}
